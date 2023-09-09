55160f7a98fe78f42ce3fec8d4518c0fb8b6545e
My Name <ivan_1337_qwerty_339@mail.ru>
2023-09-09 10:14:09 +0300

    Forgotten mistake on deploy

--- src/page/homePage/HomePage.tsx
+++ src/page/homePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import styles from './HomePage.module.css'
 import { Link } from "react-router-dom";
-import UIIndicators from "../../UI/indicators/UIIndicators";
 const HomePage = () => {
         
         
